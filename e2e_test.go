8ad2cdafff709427a9d5fb8c0e6bda207da4cc57
docmerlin (j. Emrys Landivar) <landivar@gmail.com>
2019-05-30 14:58:18 -0500

    WIP

--- e2e_test.go
+++ e2e_test.go
@@ -119,9 +119,8 @@ func TestE2E(t *testing.T) {
 
 	fmt.Println(sRes.Auth.Token)
 	time.Sleep(5 * time.Second)
-	r, err := influx.QueryCSV(context.Background(), `from(bucket:bucket)|>range(start:-1000h)|>group()`, `e2e-test-org`, struct{ bucket string }{bucket: "e2e-test-bucket"})
+	r, err := influx.QueryCSV(context.Background(), `from(bucket:bucket)|>range(start:-1000h)|>group()`, `e2e-test-org`, struct{ Bucket string `flux:"bucket"`}{Bucket: "e2e-test-bucket"})
 	if err != nil {
-		t.Fatal(err)
 		t.Fatal(err)
 	}
 	b, err := ioutil.ReadAll(r)

8becd7eac4748c827d85ec6392a02bb2893ebf3b
Yohan <yohan.couppey@blacknut.com>
2022-03-04 10:49:38 +0100

    feat: add User-Agent header overwrite possibility

--- api/http/service.go
+++ api/http/service.go
@@ -44,6 +44,10 @@ type Service interface {
 	SetAuthorization(authorization string)
 	// Authorization returns current authorization header value
 	Authorization() string
+	// SetUserAgent sets the User-Agent header value
+	SetUserAgent(authorization string)
+	// UserAgent returns current User-Agent header value
+	UserAgent() string
 	// ServerAPIURL returns URL to InfluxDB2 server API space
 	ServerAPIURL() string
 	// ServerURL returns URL to InfluxDB2 server
@@ -55,6 +59,7 @@ type service struct {
 	serverAPIURL  string
 	serverURL     string
 	authorization string
+	userAgent     string
 	client        Doer
 }
 
@@ -92,6 +97,14 @@ func (s *service) Authorization() string {
 	return s.authorization
 }
 
+func (s *service) SetUserAgent(userAgent string) {
+	s.userAgent = userAgent
+}
+
+func (s *service) UserAgent() string {
+	return s.userAgent
+}
+
 func (s *service) DoPostRequest(ctx context.Context, url string, body io.Reader, requestCallback RequestCallback, responseCallback ResponseCallback) *Error {
 	return s.doHTTPRequestWithURL(ctx, http.MethodPost, url, body, requestCallback, responseCallback)
 }
@@ -127,8 +140,13 @@ func (s *service) DoHTTPRequestWithResponse(req *http.Request, requestCallback R
 	if len(s.authorization) > 0 {
 		req.Header.Set("Authorization", s.authorization)
 	}
+
 	if req.Header.Get("User-Agent") == "" {
-		req.Header.Set("User-Agent", http2.UserAgent)
+		if len(s.userAgent) > 0 {
+			req.Header.Set("User-Agent", s.userAgent)
+		} else {
+			req.Header.Set("User-Agent", http2.UserAgent)
+		}
 	}
 	if requestCallback != nil {
 		requestCallback(req)
